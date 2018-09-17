89bce12cbfef5921c7f0f5d20a9d98827b895e12
Onur Genes <onur@onurgenes.com>
2018-09-17 23:05:55 +0300

    Swift 4.2 Support

--- CHIPageControl/Core/CHIBasePageControl.swift
+++ CHIPageControl/Core/CHIBasePageControl.swift
@@ -116,7 +116,7 @@ import UIKit
     
     internal func setupDisplayLink() {
         self.displayLink = CADisplayLink(target: WeakProxy(self), selector: #selector(updateFrame))
-        self.displayLink?.add(to: .current, forMode: .commonModes)
+        self.displayLink?.add(to: .current, forMode: .common)
     }
 
     @objc internal func updateFrame() {
@@ -199,7 +199,7 @@ import UIKit
     }
 
     deinit {
-        self.displayLink?.remove(from: .current, forMode: .commonModes)
+        self.displayLink?.remove(from: .current, forMode: .common)
         self.displayLink?.invalidate()
     }
 }
