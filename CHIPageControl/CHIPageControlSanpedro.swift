5c457b694e10d27b3559affd539d897ac9eda331
Peter Suwara <psuwara@realityworks.com.au>
2019-08-09 16:02:15 +0300

    Initial working new control. Sizes close to the expected result but position is not yet correct

---
+++ CHIPageControl/CHIPageControlSanpedro.swift
@@ -0,0 +1,123 @@
+//
+//  CHIPageControlSanpedro.swift
+//  CHIPageControl  ( https://github.com/ChiliLabs/CHIPageControl )
+//
+//  Copyright (c) 2019 Peter Suwara
+//
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+open class CHIPageControlSanpedro: CHIBasePageControl {
+    fileprivate let multScalar = 8.0
+    
+    fileprivate var diameter: CGFloat {
+        return radius * 2
+    }
+
+    fileprivate var inactive = [CHILayer]()
+
+    fileprivate var active: CHILayer = CHILayer()
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    override func updateNumberOfPages(_ count: Int) {
+        inactive.forEach { $0.removeFromSuperlayer() }
+        inactive = [CHILayer]()
+        inactive = (0...count).map {_ in
+            let layer = CHILayer()
+            self.layer.addSublayer(layer)
+            return layer
+        }
+        self.layer.addSublayer(active)
+
+        setNeedsLayout()
+        self.invalidateIntrinsicContentSize()
+    }
+
+    override func update(for progress: Double) {
+        guard progress >= 0 && progress <= Double(numberOfPages - 1),
+            let firstFrame = self.inactive.first?.frame,
+            numberOfPages > 1 else { return }
+
+        let normalized = progress * Double(diameter + padding)
+        let distance = abs(round(progress) - progress)
+        
+        print("Normalized : \(normalized)")
+        print("Distance : \(distance)")
+        
+        let mult = 1 + distance * multScalar
+        print("Mult : \(mult)")
+
+        var frame = active.frame
+
+        frame.origin.x = CGFloat(normalized) + firstFrame.origin.x
+        frame.size.width = (frame.height * 2 + padding) * (1/CGFloat(mult))
+        frame.size.height = self.diameter
+
+        active.frame = frame
+    }
+
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        
+        print ("Updating subviews")
+        
+        let floatCount = CGFloat(inactive.count)
+        
+        print ("floatCount : \(floatCount)")
+        let x = (self.bounds.size.width - self.diameter*floatCount - self.padding*(floatCount-1))*0.5
+        let y = (self.bounds.size.height - self.diameter)*0.5
+        print ("X : \(x)")
+        print ("Y : \(y)")
+        var frame = CGRect(x: x, y: y, width: self.diameter, height: self.diameter)
+
+        active.cornerRadius = self.radius
+        active.backgroundColor = (self.currentPageTintColor ?? self.tintColor)?.cgColor
+        active.frame = frame
+
+        inactive.enumerated().forEach() { index, layer in
+            layer.backgroundColor = self.tintColor(position: index).withAlphaComponent(self.inactiveTransparency).cgColor
+            if self.borderWidth > 0 {
+                layer.borderWidth = self.borderWidth
+                layer.borderColor = self.tintColor(position: index).cgColor
+            }
+            layer.cornerRadius = self.radius
+            layer.frame = frame
+            frame.origin.x += self.diameter + self.padding
+        }
+        update(for: progress)
+    }
+
+    override open var intrinsicContentSize: CGSize {
+        return sizeThatFits(CGSize.zero)
+    }
+
+    override open func sizeThatFits(_ size: CGSize) -> CGSize {
+        return CGSize(width: CGFloat(inactive.count) * self.diameter + CGFloat(inactive.count - 1) * self.padding,
+                      height: self.diameter)
+    }
+}
