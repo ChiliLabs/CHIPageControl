df110b1bdfcf875f4a8b0a3109a86a44b2f8133a
Mohammad Porooshani <porooshani@gmail.com>
2020-01-19 13:36:10 +0330

    Updated Source Paths
    Signed-off-by: Mohammad Porooshani <porooshani@gmail.com>

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
     dependencies: [],
     targets: [
         .target(name: "CHIPageControl", path: "CHIPageControl/Core"),
-        .target(name: "Aji", dependencies: ["CHIPageControl"], sources: ["CHIPageControl/CHIPageControlAji.swift"]),
-        .target(name: "Aleppo", dependencies: ["CHIPageControl"], sources: ["CHIPageControl/CHIPageControlAleppo.swift"]),
-        .target(name: "Chimayo", dependencies: ["CHIPageControl"], sources: ["CHIPageControl/CHIPageControlChimayo.swift"]),
-        .target(name: "Fresno", dependencies: ["CHIPageControl"], sources: ["CHIPageControl/CHIPageControlFresno.swift"]),
-        .target(name: "Jalapeno", dependencies: ["CHIPageControl"], sources: ["CHIPageControl/CHIPageControlJalapeno.swift"]),
-        .target(name: "Jaloro", dependencies: ["CHIPageControl"], sources: ["CHIPageControl/CHIPageControlJaloro.swift"]),
-        .target(name: "Paprika", dependencies: ["CHIPageControl"], sources: ["CHIPageControl/CHIPageControlPaprika.swift"]),
-        .target(name: "Puya", dependencies: ["CHIPageControl"], sources: ["CHIPageControl/CHIPageControlPuya.swift"])
+        .target(name: "Aji", dependencies: ["CHIPageControl"], path: "CHIPageControl", sources: ["CHIPageControlAji.swift"]),
+        .target(name: "Aleppo", dependencies: ["CHIPageControl"], path: "CHIPageControl", sources: ["CHIPageControlAleppo.swift"]),
+        .target(name: "Chimayo", dependencies: ["CHIPageControl"], path: "CHIPageControl", sources: ["CHIPageControlChimayo.swift"]),
+        .target(name: "Fresno", dependencies: ["CHIPageControl"], path: "CHIPageControl", sources: ["CHIPageControlFresno.swift"]),
+        .target(name: "Jalapeno", dependencies: ["CHIPageControl"], path: "CHIPageControl", sources: ["CHIPageControlJalapeno.swift"]),
+        .target(name: "Jaloro", dependencies: ["CHIPageControl"], path: "CHIPageControl", sources: ["CHIPageControlJaloro.swift"]),
+        .target(name: "Paprika", dependencies: ["CHIPageControl"], path: "CHIPageControl", sources: ["CHIPageControlPaprika.swift"]),
+        .target(name: "Puya", dependencies: ["CHIPageControl"], path: "CHIPageControl", sources: ["CHIPageControlPuya.swift"])
         
     ],
     swiftLanguageVersions: [.v5]
