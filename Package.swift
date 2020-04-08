8c3345af0c06c11998f94fe80b0dfddc67b4c78f
th-mustache-dk <58849846+th-mustache-dk@users.noreply.github.com>
2020-04-08 11:09:44 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+  name: "CHIPageControl",
+  platforms: [
+    .iOS(.v8)
+  ],
+  products: [
+    .library(name: "CHIPageControl", targets: ["CHIPageControl"]),
+  ],
+  targets: [
+    .target(name: "CHIPageControl")
+  ]
+)
