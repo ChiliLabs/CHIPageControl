ee9e6480d1772a714bbab4992f84ca4153d21e98
Saad Aamir <saad-aamir@live.com>
2021-10-07 12:19:33 +0300

    IPHONEOS_DEPLOYMENT_TARGET set to 12.2
    Signed-off-by: Saad Aamir <saad-aamir@live.com>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "CHIPageControl",
   platforms: [
-    .iOS(.v8)
+    .iOS(.v12)
   ],
   products: [
     .library(name: "CHIPageControl", targets: ["CHIPageControl"]),
