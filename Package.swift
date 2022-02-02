543ad155731ed5b17bc36889d1b77e547e3cb8c9
Serhan Aksüt <serhanaksut@gmail.com>
2022-02-02 16:34:36 +0100

    SPM support has been implemented. (#78)
    - Package.swift file has been added and configured.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.4
+
+import PackageDescription
+
+let package = Package(
+    name: "CHIPageControl",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "CHIPageControl",
+            targets: ["CHIPageControl"]
+        )
+    ],
+    targets: [
+        .target(name: "CHIPageControl", path: "CHIPageControl")
+    ]
+)
